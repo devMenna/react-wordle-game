d28353ace2786c55dd4dac084e77db967e7727e8
DevMenna <dev.mennaalaa@gmail.com>
2022-10-08 16:58:36 +0200

    tracking and formatting user's input

---
+++ src/components/Wordle.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { useEffect } from "react";
+import useWordle from "../hooks/useWordle";
+
+const Wordle = ({solution}) => {
+    const { currentGuess , handleKeyup } = useWordle(solution)
+
+    useEffect(() => {
+        window.addEventListener('keyup' , handleKeyup) 
+
+        return () => window.removeEventListener('keyup' , handleKeyup)
+    } , [handleKeyup])
+
+    return ( 
+        <div>
+            <div>solution - { solution }</div>
+            <div>current guess - { currentGuess }</div>
+        </div>
+     );
+}
+ 
+export default Wordle;
