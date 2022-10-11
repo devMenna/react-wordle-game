21a2d915eb15802ed6809d5592e2c483c05db086
devMenna <dev.mennaalaa@gmail.com>
2022-10-11 05:52:51 +0200

    adding keypad to track guessed letters

--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -2,25 +2,35 @@ import React from "react";
 import { useEffect } from "react";
 import useWordle from "../hooks/useWordle";
 import Grid from "./Grid";
+import Keypad from "./Keypad";
 
 const Wordle = ({solution}) => {
-    const { currentGuess , handleKeyup , guesses , isCorrect , turn } = useWordle(solution)
+    const { currentGuess , handleKeyup , guesses , isCorrect ,usedKeys, turn } = useWordle(solution)
 
     useEffect(() => {
         window.addEventListener('keyup' , handleKeyup) 
 
-        return () => window.removeEventListener('keyup' , handleKeyup)
-    } , [handleKeyup])
+        if(isCorrect){
+            console.log('congrats, you won!')
+            window.removeEventListener('keyup' , handleKeyup)
+        }
 
-    useEffect(() => {
-        console.log(guesses , turn , isCorrect)
-    } , [guesses , turn , isCorrect])
+        if(turn > 5){
+            console.log('unlucky, you lose!')
+            window.removeEventListener('keyup' , handleKeyup)
+        }
+
+        return () => window.removeEventListener('keyup' , handleKeyup)
+    } , [handleKeyup , isCorrect])
+
+
 
     return ( 
         <div>
             <div>solution - { solution }</div>
             <div>current guess - { currentGuess }</div>
             <Grid currentGuess={ currentGuess } guesses={ guesses } turn={ turn } />
+            <Keypad usedKeys={usedKeys} />
         </div>
      );
 }
